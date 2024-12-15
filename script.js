83f426222e8fdaa727f0fdaf12708882f35ea02e
Aston <a.killett@yahoo.co.uk>
2024-12-15 20:53:16 +0000

    Fixes

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ window.onload = function () {
     const fadeInElement = document.querySelector('.fade-in-text');
     fadeInElement.classList.add('fade-in');
 };
+
+let mybutton = document.getElementById("scrolltopbutton");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
